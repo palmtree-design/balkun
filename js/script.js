6e7bd924f37aeaad2f6a006d46619a381f49681c
palmtree-design <130032978+palmtree-design@users.noreply.github.com>
2023-07-06 22:23:39 +0900

    js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,5 @@
 document.querySelector('.MenuBtn').addEventListener('click', function(){
     document.querySelector('.hbg-menu').classList.toggle('is-active');
     document.querySelector('.MenuBtn').classList.toggle('isClosed');
-    document.querySelector('.loop').classList.toggle('isClosed');
 });
 
