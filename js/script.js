c5ede25db47a6576eff03ee873943830831d627a
palmtree-design <130032978+palmtree-design@users.noreply.github.com>
2023-07-06 23:11:27 +0900

    スクリプト

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ document.querySelector('.MenuBtn').addEventListener('click', function(){
     document.querySelector('.MenuBtn').classList.toggle('isClosed');
 });
 
+document.querySelector('.Menu-Group-Item').addEventListener('click', function(){
+    document.querySelector('.hbg-menu').classList.remove('is-active');
+    document.querySelector('.MenuBtn').classList.remove('isClosed');
+});
